ad6c968e1ed44772c292dcfb98b3fd330af823df
bastian-alvarez <ba.alvarezb@duocuc.cl>
2025-05-06 14:36:27 -0400

    primer guardado del proyecto github

---
+++ proyectogithub/src/main/java/com/example/proyectogithub/ProyectogithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectogithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectogithubApplication.class, args);
+	}
+
+}
